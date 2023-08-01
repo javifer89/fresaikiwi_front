1c3023c952ed2a70d670cf580cbfa70d73501a09
javifer89 <122439902+javifer89@users.noreply.github.com>
2023-08-02 01:22:19 +0200

    componentes, servicios, rutas generados , instaladas librerias fullCalendar, bootstrap, jwt.-decode

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { AboutComponent } from './components/about/about.component';
+import { SesionesComponent } from './components/sesiones/sesiones.component';
+import { BlogComponent } from './components/blog/blog.component';
+import { ReservasComponent } from './components/reservas/reservas.component';
+import { ContactoComponent } from './components/contacto/contacto.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    AboutComponent,
+    SesionesComponent,
+    BlogComponent,
+    ReservasComponent,
+    ContactoComponent
   ],
   imports: [
     BrowserModule,
