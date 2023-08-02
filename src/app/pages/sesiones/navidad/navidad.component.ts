6cb95f7c4dafa043d96765109b7ae0e3f964fcf0
javifer89 <122439902+javifer89@users.noreply.github.com>
2023-08-02 23:29:22 +0200

    creados los componentes para cada sesion dentro de la carpeta SESIONES de la carpeta  PAGES

---
+++ src/app/pages/sesiones/navidad/navidad.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-navidad',
+  templateUrl: './navidad.component.html',
+  styleUrls: ['./navidad.component.css']
+})
+export class NavidadComponent {
+
+}
