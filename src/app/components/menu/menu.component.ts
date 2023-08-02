499589339891178c7435b8dcb46c50207c907216
javifer89 <122439902+javifer89@users.noreply.github.com>
2023-08-02 23:31:25 +0200

    empezado el componente menu

--- src/app/components/menu/menu.component.ts
+++ src/app/components/menu/menu.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { Router } from '@angular/router';
+// import { StaffService } from 'src/app/services/staff.service';
+// import { UsuariosService } from 'src/app/services/usuarios.service';
 
 @Component({
   selector: 'app-menu',
   templateUrl: './menu.component.html',
-  styleUrls: ['./menu.component.css']
+  styleUrls: ['./menu.component.css'],
 })
 export class MenuComponent {
+  // staffService = inject(StaffService);
 
+  // usuariosService = inject(UsuariosService);
+
+  router = inject(Router);
+
+  onLogout(logOut: number) {
+    localStorage.removeItem('token_front');
+    if (logOut === 0) {
+      this.router.navigate(['/loginStaff']);
+    } else {
+      this.router.navigate(['/usuarios', 'login']);
+    }
+  }
 }
