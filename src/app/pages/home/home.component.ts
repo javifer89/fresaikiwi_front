a03748951360c4c2f54e84e54d71ec7b4725398c
javifer89 <122439902+javifer89@users.noreply.github.com>
2023-08-08 12:47:42 +0200

    configurando slider parte superior, falta darle más tamaño todavia

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -7,51 +7,51 @@ import { CarouselModule } from 'ngx-bootstrap/carousel';
   styleUrls: ['./home.component.css'],
 })
 export class HomeComponent implements OnInit {
-  // images: any[];
+   images: any[];
   imagenSeleccionada: number = 0;
   interval: any;
 
-  images = [
-    '../../../assets/images/NEWBORN/EIRE-35 copia.jpg',
-    '../../../assets/images/COMUNIONES/EDURNE-74 copia.jpg',
-    '../../../assets/images/CUMPLE/CARLOTA_2ANYS-147 còpia copia.jpg',
-    '../../../assets/images/BEBÉS/ALEJANDRI_9M-43.jpg',
-    '../../../assets/images/COMUNIONES/SYRA_EXTERIORS-69 copia.jpg',
-    '../../../assets/images/EMBARAZO/EUGE_EMB-8.jpg',
-  ];
+  // images = [
+  //   '../../../assets/images/NEWBORN/EIRE-35 copia.jpg',
+  //   '../../../assets/images/COMUNIONES/EDURNE-74 copia.jpg',
+  //   '../../../assets/images/CUMPLE/CARLOTA_2ANYS-147 còpia copia.jpg',
+  //   '../../../assets/images/BEBÉS/ALEJANDRI_9M-43.jpg',
+  //   '../../../assets/images/COMUNIONES/SYRA_EXTERIORS-69 copia.jpg',
+  //   '../../../assets/images/EMBARAZO/EUGE_EMB-8.jpg',
+  // ];
   constructor() {
-    // this.images = [
-    //   {
-    //     nombre: 'Sala de conferencias',
-    //     url: '../../../assets/images/NEWBORN/EIRE-35 copia.jpg',
-    //     descripcion: 'Sala de conferencias',
-    //   },
-    //   {
-    //     nombre: 'Sala de conciertos',
-    //     url: '../../../assets/images/COMUNIONES/EDURNE-74 copia.jpg',
-    //     descripcion: 'Sala de conciertos Hotel Hacienda de Cortés',
-    //   },
-    //   {
-    //     nombre: 'Sala de exposiciones',
-    //     url: '../../../assets/images/CUMPLE/CARLOTA_2ANYS-147 còpia copia.jpg',
-    //     descripcion: 'Sala de exposiciones',
-    //   },
-    //   {
-    //     nombre: 'Sala de formación',
-    //     url: '../../../assets/images/BEBÉS/ALEJANDRI_9M-43.jpg',
-    //     descripcion: 'Sala de formación Da Vinci',
-    //   },
-    //   {
-    //     nombre: 'Teatro Jovellanos',
-    //     url: '../../../assets/images/COMUNIONES/SYRA_EXTERIORS-69 copia.jpg',
-    //     descripcion: 'Teatro Jovellanos de Gijón',
-    //   },
-    //   {
-    //     nombre: 'Sala de conferencias Chachi',
-    //     url: '../../../assets/images/EMBARAZO/EUGE_EMB-8.jpg',
-    //     descripcion: 'Teatro Jovellanos de Gijón',
-    //   },
-    // ];
+    this.images = [
+      {
+        nombre: 'Sala de conferencias',
+        url: '../../../assets/images/NEWBORN/EIRE-35 copia.jpg',
+        descripcion: 'Sala de conferencias',
+      },
+      {
+        nombre: 'Sala de conciertos',
+        url: '../../../assets/images/COMUNIONES/EDURNE-74 copia.jpg',
+        descripcion: 'Sala de conciertos Hotel Hacienda de Cortés',
+      },
+      {
+        nombre: 'Sala de exposiciones',
+        url: '../../../assets/images/CUMPLE/CARLOTA_2ANYS-147 còpia copia.jpg',
+        descripcion: 'Sala de exposiciones',
+      },
+      {
+        nombre: 'Sala de formación',
+        url: '../../../assets/images/BEBÉS/ALEJANDRI_9M-43.jpg',
+        descripcion: 'Sala de formación Da Vinci',
+      },
+      {
+        nombre: 'Teatro Jovellanos',
+        url: '../../../assets/images/COMUNIONES/SYRA_EXTERIORS-69 copia.jpg',
+        descripcion: 'Teatro Jovellanos de Gijón',
+      },
+      {
+        nombre: 'Sala de conferencias Chachi',
+        url: '../../../assets/images/EMBARAZO/EUGE_EMB-8.jpg',
+        descripcion: 'Teatro Jovellanos de Gijón',
+      },
+    ];
   }
 
   ngOnInit(): void {
