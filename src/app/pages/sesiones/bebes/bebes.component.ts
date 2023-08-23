73695322680936f0b7224433390cad1e1b5cf79f
javifer89 <122439902+javifer89@users.noreply.github.com>
2023-08-23 23:52:51 +0200

    añadido lightbox a Bebés y generado styles SCSS - falta aplicar al resto de sesiones

--- src/app/pages/sesiones/bebes/bebes.component.ts
+++ src/app/pages/sesiones/bebes/bebes.component.ts
@@ -3,8 +3,30 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-bebes',
   templateUrl: './bebes.component.html',
-  styleUrls: ['./bebes.component.css']
+  styleUrls: ['./bebes.component.scss']
 })
 export class BebesComponent {
+  imagenes: GridImg[] = [];
+  lightboxActive: boolean = false;
+  imgIndex: number = 0;
 
+  gridImgHandler(index: number) {
+    this.lightboxActive = true;
+    this.imgIndex = index
+  }
+  lightboxBtnHandler() {
+    this.lightboxActive = false;
+  }
+  constructor() {
+    this.imagenes = [
+      { src: '/assets/images/BEBÉS/P1_FILES_EMMA_BATHMILK-5.jpg', alt: 'imagen 0' },
+      { src: '/assets/images/BEBÉS/ALEJANDRI_9M-43.jpg', alt: 'imagen 1' },
+      { src: '/assets/images/BEBÉS/ALEJANDRI_9M-43.jpg', alt: 'imagen 2' },
+    ];
+  }
+}
+
+interface GridImg{
+  src: string,
+  alt: string
 }
