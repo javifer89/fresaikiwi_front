7fe6db1dcd1b9c46e714f9b9bb60c468e847d740
javifer89 <122439902+javifer89@users.noreply.github.com>
2023-09-05 20:28:35 +0200

    ajustes en sesion bebés

--- src/app/pages/sesiones/bebes/bebes.component.ts
+++ src/app/pages/sesiones/bebes/bebes.component.ts
@@ -3,23 +3,28 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-bebes',
   templateUrl: './bebes.component.html',
-  styleUrls: ['./bebes.component.scss']
+  styleUrls: ['./bebes.component.scss'],
 })
 export class BebesComponent {
   imagenes: GridImg[] = [];
   lightboxActive: boolean = false;
   imgIndex: number = 0;
+ 
 
   gridImgHandler(index: number) {
     this.lightboxActive = true;
-    this.imgIndex = index
+    this.imgIndex = index;
   }
+
   lightboxBtnHandler() {
     this.lightboxActive = false;
   }
   constructor() {
     this.imagenes = [
-      { src: '/assets/images/BEBÉS/P1_FILES_EMMA_BATHMILK-5.jpg', alt: 'imagen 0' },
+      {
+        src: '/assets/images/BEBÉS/P1_FILES_EMMA_BATHMILK-5.jpg',
+        alt: 'imagen 0',
+      },
       { src: '/assets/images/BEBÉS/ALEJANDRI_9M-43.jpg', alt: 'imagen 1' },
       { src: '/assets/images/BEBÉS/ALEJANDRI_9M-43.jpg', alt: 'imagen 2' },
     ];
