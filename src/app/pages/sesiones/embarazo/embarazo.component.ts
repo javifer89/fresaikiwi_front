0a7334dc4949c4ade66cad41f8d9325532f391d8
javifer89 <122439902+javifer89@users.noreply.github.com>
2023-09-26 15:52:41 +0200

    scrolltoContainer añadido

--- src/app/pages/sesiones/embarazo/embarazo.component.ts
+++ src/app/pages/sesiones/embarazo/embarazo.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-embarazo',
@@ -6,10 +6,15 @@ import { Component } from '@angular/core';
   styleUrls: ['./embarazo.component.scss']
 })
 export class EmbarazoComponent {
- imagenes: GridImg[] = [];
+  imagenes: GridImg[] = [];
   lightboxActive: boolean = false;
   imgIndex: number = 0;
 
+  @HostListener('document:keydown.escape', ['$event'])
+  handleEscapeKey(event: KeyboardEvent) {
+    this.lightboxActive = false;
+
+  }
   gridImgHandler(index: number) {
     this.lightboxActive = true;
     this.imgIndex = index
@@ -20,20 +25,26 @@ export class EmbarazoComponent {
   constructor() {
     this.imagenes = [
       {
-        src: '/assets/images/EMBARAZO/ELENA_EMB-134 copia 2.jpg',
+        src: 'https://res.cloudinary.com/dscycaajk/image/upload/v1695592850/fresaikiwi/embarazo/bv3dudt2czkphmqdsj6l.jpg',
         alt: 'imagen 0',
       },
+      { src: 'https://res.cloudinary.com/dscycaajk/image/upload/v1695592850/fresaikiwi/embarazo/shgjkzw9s98ec1ywv2gw.jpg', alt: 'imagen 1' },
+      { src: 'https://res.cloudinary.com/dscycaajk/image/upload/v1695592852/fresaikiwi/embarazo/avqyfoplwyas7z5bidp2.jpg', alt: 'imagen 2' },
       {
-        src: '/assets/images/EMBARAZO/ELENA_EMB-247 copia 2.jpg',
-        alt: 'imagen 1',
+        src: 'https://res.cloudinary.com/dscycaajk/image/upload/v1695592851/fresaikiwi/embarazo/tzwfys8otbdbmgl5uozg.jpg',
+        alt: 'imagen 3',
       },
-      { src: '/assets/images/EMBARAZO/SONIETES-5.jpg', alt: 'imagen 2' },
-      { src: '/assets/images/EMBARAZO/EUGE_EMB-64.jpg', alt: 'imagen 3' },
     ];
+  }
+  // TODO REVISAR AREA del FONDO CLICKABLE PARA SALIR de la imagen
+  closeLightboxOnClick(event: Event) {
+    if (event.target === event.currentTarget) {
+      this.lightboxActive = false;
+    }
   }
 }
 
-interface GridImg{
+interface GridImg {
   src: string,
   alt: string
 }
