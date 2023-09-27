875ff414452e2959a08dc3fee4b5256c1370d2cd
javifer89 <122439902+javifer89@users.noreply.github.com>
2023-09-27 14:06:08 +0200

    ajustes en HOME, falta eliminar el autoscroll en la home para que se vea el slider

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -6,49 +6,59 @@ import { Router } from '@angular/router';
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css'],
 })
-export class HomeComponent implements OnInit {
-  images: any[];
-  imagenSeleccionada: number = 0;
-  interval: any;
-
+export class HomeComponent {
+// export class HomeComponent implements OnInit {
+  // images: any[];
+  // imagenSeleccionada: number = 0;
+  // interval: any;
+  // TODO SOLUCIONAR SCROLL (NO HACER SCROLL EN LA HOME)
+scrollToComponent= false
   router: Router = inject(Router)
+  // sesionesComponent: any;
 
-    constructor() {
-    this.images = [
-      {
-        nombre: 'Newborn',
-        url: '../../../assets/images/NEWBORN/EIRE-35 copia.jpg',
-      },
-      {
-        nombre: 'Comunión',
-        url: '../../../assets/images/COMUNIONES/EDURNE-74 copia.jpg',
-      },
-      {
-        nombre: 'Cumpleaños',
-        url: '../../../assets/images/CUMPLE/CARLOTA_2ANYS-147 còpia copia.jpg',
-      },
-      {
-        nombre: 'Bebés',
-        url: '../../../assets/images/BEBÉS/ALEJANDRI_9M-43.jpg',
-      },
-      ];
+  constructor() {
+    // Establece scrollToComponent en false para evitar el scroll en la ruta Home
+    // this.sesionesComponent.scrollToComponent = false;
+    
+    // this.images = [
+    //   {
+    //     nombre: 'Newborn',
+    //     url: '../../../assets/images/NEWBORN/EIRE-35 copia.jpg',
+    //   },
+    //   {
+    //     nombre: 'Comunión',
+    //     url: '../../../assets/images/COMUNIONES/ANNA_COMU.jpg',
+    //   },
+    //   {
+    //     nombre: 'Cumpleaños',
+    //     url: '../../../assets/images/CUMPLE/CARLOTA_2ANYS-147 còpia copia.jpg',
+    //   },
+    //   {
+    //     nombre: 'Bebés',
+    //     url: '../../../assets/images/BEBÉS/ALEJANDRI_9M-43.jpg',
+    //   },
+    //   {
+    //     nombre: 'Embarazo',
+    //     url: '../../../assets/images/EMBARAZO/MARTETA_EMB-15 copia.jpg',
+    //   },
+    // ];
   }
 
-  ngOnInit(): void {
-    this.startInterval();
-  }
+  // ngOnInit(): void {
+  //   this.startInterval();
+  // }
 
-  cambiaImagen() {
-    this.imagenSeleccionada++;
-    if (this.imagenSeleccionada >= this.images.length) {
-      this.imagenSeleccionada = 0;
-    }
-  }
-  startInterval() {
-    this.interval = setInterval(() => {
-      this.cambiaImagen();
-    }, 5000);
-  }
+  // cambiaImagen() {
+  //   this.imagenSeleccionada++;
+  //   if (this.imagenSeleccionada >= this.images.length) {
+  //     this.imagenSeleccionada = 0;
+  //   }
+  // }
+  // startInterval() {
+  //   this.interval = setInterval(() => {
+  //     this.cambiaImagen();
+  //   }, 5000);
+  // }
 }
   // stopInterval() {
   //   clearInterval(this.interval);
